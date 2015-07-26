34a780c1c16c98cc59f197d892584d081c42bd2c
Manfred Touron <m@42.am>
2015-07-27 01:06:36 +0200

    Initial version

---
+++ millipede.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  unsigned int i;
+  size_t size = 20;
+  char *padding_offsets[8] = {"  ", " ", "", " ", "  ", "   ", "    ", "   "};
+
+  if (argc > 1) {
+    size = atoi(argv[1]);
+  }
+
+  printf("    ╚⊙ ⊙╝\n");
+  for (i = 0; i < size; i++) {
+    printf("%s╚═(███)═╝\n", padding_offsets[i % 8]);
+  }
+  return 0;
+}
